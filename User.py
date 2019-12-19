217be04b913dbbf98d94367f11aa900c459e5f53
Michal Kruszewski <kruszewski2@gmail.com>
2019-12-19 12:46:15 +0100

    Added function which parse user to dictionary

--- User.py
+++ User.py
@@ -7,3 +7,12 @@ class User:
 
     def __str__(self):
         return "User: {0}, email: {1}".format(self.name, self.email)
+
+
+    def get_user_as_dict(self):
+        result = {
+            "name": self.name,
+            "password": self.password,
+            "email": self.email
+        }
+        return result
