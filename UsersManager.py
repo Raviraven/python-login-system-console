99207997ade511dd7c224cfb15ee9dc2326f1640
Michal Kruszewski <kruszewski2@gmail.com>
2019-12-19 13:29:37 +0100

    Added add user parametrized method

--- UsersManager.py
+++ UsersManager.py
@@ -7,5 +7,10 @@ def add_user(new_user):
     usersDB.append(new_user)
 
 
+def add_user_parametrized(name, password, email):
+    new_user = User(name=name, pwd=password, email=email)
+    add_user(new_user)
+
+
 def get_users():
     return usersDB
