a5320a14efb707fe7c801e024318fa00de96d191
Michal Kruszewski <kruszewski2@gmail.com>
2019-12-19 09:52:51 +0100

    Added Login System file

---
+++ LoginSystem.py
@@ -0,0 +1,7 @@
+print("Hi! Do you want to register a new account or log in into existing one?")
+print("1. Register")
+print("2. Log in")
+
+chosenOption = input("Chosen option: ")
+print(chosenOption)
+
