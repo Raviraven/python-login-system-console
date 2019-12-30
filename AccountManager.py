d9c1e5d87f1531ca77220865acb504600ca2cb93
Michal Kruszewski <kruszewski2@gmail.com>
2019-12-30 15:39:12 +0100

    Added email validation by regex

--- AccountManager.py
+++ AccountManager.py
@@ -14,6 +14,7 @@ def register_account(name, password, repeat_password, email):
     try:
         username_validation(name)
         password_validation(password, repeat_password)
+        email_validation(email)
 
         new_user = User.User(name=name, pwd=password, email=email)
         add_user(new_user)
@@ -48,6 +49,7 @@ def password_validation(password, repeat_password):
 
 
 def email_validation(email):
-    match = re.fullmatch("pattern", email)
+    email_pattern = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    match = re.fullmatch(email_pattern, email)
     if match is None:
         raise InvalidEmailException
