0adc076f474587d4f894111a552fc9200e844503
Michal Kruszewski <kruszewski2@gmail.com>
2020-01-03 11:47:28 +0100

    Login system prepared for creating exe

--- LoginSystem.py
+++ LoginSystem.py
@@ -17,8 +17,8 @@ def get_input(input_prompt):
 
 def register():
     new_username = input("Your username: ")
-    new_password = input("Password: ")  # getpass(prompt="Password: ")  # will work outside pycharm
-    repeat_password = input("Repeat password: ")  # getpass(prompt="Repeat_password: ")
+    new_password = getpass(prompt="Password: ") # input("Password: ")   # will work outside pycharm
+    repeat_password = getpass(prompt="Repeat_password: ") # input("Repeat password: ")
     new_email = input("Email: ")
 
     AccountManager.register_account(name=new_username, password=new_password, repeat_password=repeat_password,
@@ -27,29 +27,30 @@ def register():
 
 def login():
     username = input("Enter username: ")
-    password = input("Enter password: ")
+    password = getpass("Enter password: ")
     AccountManager.login_account(username=username, password=password)
 
 
-FilesOperations.read_users_from_file()
 clear()
+FilesOperations.read_users_from_file()
 print("Hi! Do you want to register a new account or log in into existing one?")
 
 try:
     while True:
-        print("(1) Register")
+        print("\n(1) Register")
         print("(2) Log in")
         print("(q) Exit")
 
         chosenOption = get_input("Option: ")
 
         if chosenOption == '1':
+            clear()
             register()
         elif chosenOption == '2':
+            clear()
             login()
         else:
             print("wrong command")
 
-        clear()
 except ExitException as error:
     print(error)
